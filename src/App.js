e38dc9b630431ee7a4326e8d3aa25c79d16e7802
apurba2001 <apurbaruidas0358@gmail.com>
2023-02-22 19:08:20 +0530

    task completed

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useEffect, useState, useRef } from 'react'
+
+function App() {
+  const [time, setTime] = useState(0)
+  const [timeInput, setTimeInput] = useState()
+  const [start, setStart] = useState(false)
+  const timer = (e) =>{
+    if (e.key == "Enter" && timeInput > 0){
+      setTime(timeInput)
+    }
+  }
+  useEffect(() => {
+    let interval = null;
+    if (time > 0) {
+      interval = setInterval(() => setTime(prevTime => prevTime - 1), 1000);
+    }
+    return () => clearInterval(interval);
+  }, [time]);
+
+
+
+  return (
+    <div className="App">
+      <input type='number' onChange={(e) => setTimeInput(parseInt(e.target.value))} onKeyDown={timer}/>
+      <div id='current-time' >
+        {time}
+      </div>
+    </div>
+  );
+}
+
+export default App;
